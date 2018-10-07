895e0b67d142d2388d35c7254868cd4f81e1cee4
user <samvel.h2.s@tumo.org>
2018-10-07 11:55:31 +0400

    Reflesh

---
+++ class.js
@@ -0,0 +1,67 @@
+class LivingCreature {
+    constructor(x, y, index) {
+        this.x = x;
+        this.y = y;
+        this.multiply = 0;
+        this.index = index;
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1],
+        ]
+    }
+    chooseCell(ch) {
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == ch) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+}
+
+class Grass extends LivingCreature {
+    mul() {
+        this.multiply++;
+        var newCell = random(this.yntrelvandak(0));
+        if (this.multiply >= 8 && newCell) {
+            garssArr.push(newGrass);
+            matrix[newCell[1]][newCell[0]] = this.index;
+            this.multiply = 0;
+        }
+    }
+}
+
+class GrassEater extends LivingCreature {
+    constructor(x, y, index) {
+        super(x, y, index);
+        this.tariq = 0;
+        this.energy = 8;
+    }
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    choosecell(chracter) {
+        this.getNewCoordinates();
+        returnsuper.chooseCell(chracter);
+    }
+}
